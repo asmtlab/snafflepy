e34195c28a6a74a21736dfe1b4760d76e4dfc8c4
robert-todora <roberttodora@gmail.com>
2023-06-29 15:03:06 -0400

    file listing from shares working, enumeration of shares still WIP. added requirements.txt and enabled logging to stdout

--- snaffcore/go_snaffle.py
+++ snaffcore/go_snaffle.py
@@ -9,43 +9,25 @@ def begin_snaffle(options):
     print("Beginning the snaffle...")
     sleep(0.2)
     
+    # TODO: Talk to AD via LDAP to get list of computers with file shares 
 
-    # TESTING
-    if options.test:
-        server = Server('testing server', get_info=ALL)
-        conn = Connection(server, user='cn=user0,ou=test,o=lab', password='test0000', client_strategy=MOCK_SYNC)
+    # Login via SMB
+    for target in options.targets:
+        try:
+            smb_client = SMBClient(target, options.username, options.password, options.domain, options.hash)
+            smb_client.login()
+            
+            for share in smb_client.shares:
+                try:
+                    files = smb_client.ls(share, "")
 
-        conn.strategy.add_entry('cn=user0,ou=test,o=lab', {'userPassword': 'test0000', 'sn': 'user0_sn', 'revision': 0})
-        conn.strategy.add_entry('cn=user1,ou=test,o=lab', {'userPassword': 'test1111', 'sn': 'user1_sn', 'revision': 0})
-        conn.strategy.add_entry('cn=user2,ou=test,o=lab', {'userPassword': 'test2222', 'sn': 'user2_sn', 'revision': 0})
-    
-        if conn.bind():
-            print(server.info)
-        else:
-            print("Error connecting to domain")
-            print(conn.result)
-            sys.exit(2)
-    else:
-        
-        # Login through LDAP  
-        #server = Server(options.targets[0], get_info=ALL)
-        #conn = Connection(server)
-        
-        # if not conn.bind():            
-        #     print("Error connecting to domain")
-        #     print(conn.result)
-        #     sys.exit(2)
-        
-        #print(server.info)
-        
-        # Login via SMB
-        for target in options.targets:
-            try:
-                smb_client = SMBClient(target, options.username, options.password, options.domain, options.hash)
-                smb_client.login()
-                
-                for share in smb_client.shares:
-                    print(share)
+                    for file in files:
+                        print("Found file in %s: %s" % (share, file))
 
-            except Exception as e:
-                print("Exception: ", e)
+                except FileListError:
+                    print("Access Denied, cannot list files in %s" % share)
+                    continue
+
+        except Exception as e:
+            print("Exception: ", e)
+        
